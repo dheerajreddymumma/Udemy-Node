122d9cf6c258c9006d35b8fd4b6c68ab240297c5
dheerajreddymumma <dheerajreddymumma@gmail.com>
2019-12-19 20:24:42 -0800

    Adding the right key

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/67eaf36f1b8da91adf618a539e091bca/' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
